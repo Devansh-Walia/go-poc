c18ef9ef9538ed65e3e9a91001757571227fe367
THO-COR203_hdfclife <THO_COR203@HDFCLIFE.COM>
2025-05-06 15:03:25 +0530

    adding const and println with strings

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,9 +6,12 @@ import (
 )
 
 func main() {
+	const inflationRate = 4.5
 	investmentAmount, years, investmentRate := 10000.0, 10.0, 5.5
 
 	futureValue := investmentAmount * math.Pow((1+investmentRate/100), years)
+	inflationAdjustedFutureValue := futureValue / math.Pow(inflationRate/100, years)
 
-	fmt.Println(futureValue)
+	fmt.Println("future value -> ", futureValue)
+	fmt.Println("inflation adjusted future value ->", inflationAdjustedFutureValue)
 }
