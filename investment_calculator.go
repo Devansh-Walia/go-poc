369667ea51f61360b2b3cfb6e9b8721b5c6dd3d7
THO-COR203_hdfclife <THO_COR203@HDFCLIFE.COM>
2025-05-12 14:58:05 +0530

    function update

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,8 +5,9 @@ import (
 	"math"
 )
 
+const inflationRate = 4.5
+
 func main() {
-	const inflationRate = 4.5
 	var investmentAmount, years, investmentRate float64
 
 	fmt.Print("Enter the amount you want to invest: ")
@@ -16,12 +17,18 @@ func main() {
 	fmt.Print("Enter the no of years you want to invest: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow((1+investmentRate/100), years)
-	inflationAdjustedFutureValue := futureValue / math.Pow(inflationRate/100, years)
-	profit := inflationAdjustedFutureValue - investmentAmount
+	futureValue, inflationAdjustedFutureValue, profit := calculation(investmentAmount, years, investmentRate)
 
 	fmt.Println("future value -> ", futureValue)
 	fmt.Println("inflation adjusted future value ->", inflationAdjustedFutureValue)
 
 	fmt.Printf("Total profit of %.2f was achieved", profit)
+}
+
+func calculation(investmentAmount, years, investmentRate float64) (float64, float64, float64) {
+	fv := investmentAmount * math.Pow((1+investmentRate/100), years)
+	inv := fv / math.Pow(inflationRate/100, years)
+	profit := inv - investmentAmount
+
+	return fv, inv, profit
 }
